2501c2a071419ea68e90934f44a75212ed1b9319
drummermanrob20 <robert@stapleton-family.net>
2023-01-03 13:45:11 -0500

    beginnings of semantic versioning format function

--- Install-WSM.ps1
+++ Install-WSM.ps1
@@ -1,1 +1,18 @@
 # Invoke-WebRequest -Uri https://cdn.watchguard.com/SoftwareCenter/Files/WSM/12_4_1/wsm_12_4_1.exe -OutFile "C:\wsm_12_9.exe"
+
+function Format-WSMVersion {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [System.Management.Automation.SemanticVersion]$Version
+    )
+
+
+    $obj = [PSCustomObject]@{
+        Major = $Major
+        Minor = $Minor
+        Patch = $Patch
+    }
+    
+    Write-Output $obj
+}
