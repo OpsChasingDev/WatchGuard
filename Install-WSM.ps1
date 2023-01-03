331c301f6d99d91eed3af328b64d476cd5f46806
drummermanrob20 <robert@stapleton-family.net>
2023-01-03 14:18:36 -0500

    custom path for download

--- Install-WSM.ps1
+++ Install-WSM.ps1
@@ -12,9 +12,23 @@ function Format-WSMVersion {
 }
 
 function Download-WSM {
+    [CmdletBinding()]
+    param (
+        [ValidatePattern('([0-9]|[A-Z])\.exe$')]
+        [Parameter(HelpMessage = "Enter a full file path and name ending in .exe")]
+        [string]$Path
+    )
     $Version = Format-WSMVersion
+    if (!$Path) {
+        $Path = (Get-Location).Path + "\wsm_$($Version).exe"
+    }
     $Uri = "https://cdn.watchguard.com/SoftwareCenter/Files/WSM/$($Version)/wsm_$($Version).exe"
-    Invoke-WebRequest -Uri $Uri -OutFile "C:\wsm_$($Version).exe"
+    try {
+        Invoke-WebRequest -Uri $Uri -OutFile $Path -ErrorAction Stop -ErrorVariable DownloadError
+    }
+    catch {
+        Write-Output $DownloadError
+    }
 }
 
 Download-WSM
