3afd40e4194a182172512cb7ef92c8af021198b2
drummermanrob20 <robert@stapleton-family.net>
2023-01-03 14:30:53 -0500

    removed helper function

--- Install-WSM.ps1
+++ Install-WSM.ps1
@@ -1,28 +1,27 @@
-function Format-WSMVersion {
-    [CmdletBinding()]
-    param (
-        [Parameter(Mandatory)]
-        [System.Management.Automation.SemanticVersion]$Version
-    )
-    $VersionString = [string]$Version.Major + "_" + [string]$Version.Minor
-    if ($Version.Patch) {
-        $VersionString = $VersionString + "_" + $Version.Patch
-    }
-    Write-Output $VersionString
-}
-
 function Download-WSM {
     [CmdletBinding()]
     param (
+        [Parameter(Mandatory)]
+        [System.Management.Automation.SemanticVersion]$Version,
+
         [ValidatePattern('([0-9]|[A-Z])\.exe$')]
         [Parameter(HelpMessage = "Enter a full file path and name ending in .exe")]
         [string]$Path
     )
-    $Version = Format-WSMVersion
-    if (!$Path) {
-        $Path = (Get-Location).Path + "\wsm_$($Version).exe"
+
+    # format version string
+    $VersionString = [string]$Version.Major + "_" + [string]$Version.Minor
+    if ($Version.Patch) {
+        $VersionString = $VersionString + "_" + $Version.Patch
     }
-    $Uri = "https://cdn.watchguard.com/SoftwareCenter/Files/WSM/$($Version)/wsm_$($Version).exe"
+
+    # set to current location if not specified
+    if (!$Path) {
+        $Path = (Get-Location).Path + "\wsm_$($VersionString).exe"
+    }
+
+    # download
+    $Uri = "https://cdn.watchguard.com/SoftwareCenter/Files/WSM/$($VersionString)/wsm_$($VersionString).exe"
     try {
         Invoke-WebRequest -Uri $Uri -OutFile $Path -ErrorAction Stop -ErrorVariable DownloadError
     }
